5742855f0488e8d206213cff199f9b74a4bf5542
salah_elhossiny856 <salah.othman.elhossiny@gmail.com>
2022-06-10 21:20:05 +0200

    update readme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button } from "@material-ui/core";
+import { ArrowBackIosRounded  } from "@material-ui/icons";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <header className="App-header">       
+        <ArrowBackIosRounded />
+        <Button
+          color="primary"
+          variant="contained"
         >
-          Learn React
-        </a>
+          Salah hello 
+        </Button>
       </header>
     </div>
   );
 }
 
 export default App;
+
